92a7f564f033d02c745016d405935832d5e9753b
Komkris <38918147+Komkris@users.noreply.github.com>
2022-01-25 20:00:05 -0600

    Add files via upload

---
+++ HelloWorld001/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+//Console.WriteLine("Hello, World!"); // <--- Doesn't need to the code below to output "hello world" , might be a problem later on...
+
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.Read();
+        }
+    }
+}
